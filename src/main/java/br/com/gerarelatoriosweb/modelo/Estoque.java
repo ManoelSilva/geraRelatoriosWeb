d36e34333dd078e9882fbc91144e538b948c572b
Manoel Silva <manoelps.ti@gmail.com>
2016-09-28 17:47:49 -0300

    Update Estoque.java

--- src/main/java/br/com/gerarelatoriosweb/modelo/Estoque.java
+++ src/main/java/br/com/gerarelatoriosweb/modelo/Estoque.java
@@ -25,7 +25,6 @@ import javax.persistence.Table;
 @Table(name = "tbestoque")
 public class Estoque implements Serializable {
 
-    private static final long serialVersionUID = 2806421523585360625L;
     @Id
     @GeneratedValue
     private int id;
